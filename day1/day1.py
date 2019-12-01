18373234d3c77d18040296195244f817c268f4bd
Dániel Krausz <danielkrausz@Daniels-iMac.local>
2019-12-01 14:06:30 +0100

    day 1 solution

---
+++ day1/day1.py
@@ -0,0 +1,34 @@
+import math
+def main():
+    fp = open('day1.txt', 'r')
+    sum = 0
+    sum_all_modules = 0
+    line = fp.readline()
+
+    while line:
+        sum += calc_fuel(int(line))
+        sum_all_modules += calc_fuel_fuel(int(line), 0)
+        line = fp.readline()
+
+
+    fp.close()
+    #Day1 - 1 Start result
+    print(f"Day1 - 1 star solution: {sum!s}")
+    #Day1 - 2 Start result
+    print(f"Day1 - 2 star solution: {sum_all_modules!s}")
+    
+
+def calc_fuel(mass):
+    fuel = math.floor(mass / 3) - 2
+    return fuel
+
+def calc_fuel_fuel(mass, module_sum):
+    if calc_fuel(mass) <= 0:
+        return module_sum
+
+    else:
+        return calc_fuel_fuel(calc_fuel(mass), module_sum + calc_fuel(mass))
+
+
+if __name__ == "__main__":
+    main()
